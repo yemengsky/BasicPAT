64de80ef915546973d0b49027c5bbbe5a882cb13
yemengsky <yemengsky@gmail.com>
2014-02-28 18:53:22 +0800

    Update 1001.cpp

--- 1001.cpp
+++ 1001.cpp
@@ -20,3 +20,30 @@ int main(void)
 	printf("%d",i);
 	return 0;
 }
+/*
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main(void)
+{
+	long n;
+	int i=0;
+	//scanf("%d",&n);
+	cin>>n;
+	while(n!=1)
+	{
+		if(n%2==0)
+		{
+			n=n/2;
+		}
+		else
+		{
+			n=(3*n+1)/2;
+		}
+		i++;
+	}
+	//printf("%d",i);
+	cout<<i<<endl;
+	return 0;
+}
+*/
