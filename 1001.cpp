4280efd6927635026606e3900ade2f3433867549
yemengsky <yemengsky@gmail.com>
2014-02-28 18:47:06 +0800

    Update 1001.cpp
    My Answer to 1001

--- 1001.cpp
+++ 1001.cpp
@@ -1,5 +1,22 @@
 #include <stdio.h>
-void main(void)
+
+int main(void)
 {
-  return 0;
+	long n;
+	int i=0;
+	scanf("%d",&n);
+	while(n!=1)
+	{
+		if(n%2==0)
+		{
+			n=n/2;
+		}
+		else
+		{
+			n=(3*n+1)/2;
+		}
+		i++;
+	}
+	printf("%d",i);
+	return 0;
 }
