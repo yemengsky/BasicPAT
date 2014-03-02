967c1563c6ef556f945a899392a1eecca9cefd8c
yemengsky <yemengsky@gmail.com>
2014-03-02 21:11:00 +0800

    Update 1002.cpp

--- 1002.cpp
+++ 1002.cpp
@@ -4,22 +4,25 @@
 char* numstr[10]={"ling","yi","er","san","si","wu","liu","qi","ba","jiu"};
 int spltNum(int n)
 {
-	if(n>9)
-	{	
-		int temp=n%10;
-		spltNum(n/10);
-		printf(" %s",numstr[temp]);
-	}
-	else
-		printf("%s",numstr[n]);
-	return n;
+  if(n>9)
+  {  
+    int temp=n%10;
+    spltNum(n/10);
+    printf(" %s",numstr[temp]);
+  }
+  else
+    printf("%s",numstr[n]);
+  return n;
 }
 int main(void)
 {
-	char c;
-	int res=0;
-	while(scanf("%c",&c)!=EOF)
-		res+=c-'0';
-	spltNum(res);
-	return 0;
+  char c;
+  int res=0;
+  while(scanf("%c",&c)!=EOF)
+  {
+    if((c>='0')&(c<='9'))
+      res+=c-'0';
+  }
+  spltNum(res);
+  return 0;
 }
